271f42e30ae7b0c85760f5acea04339e9907aea7
Nikunj Lahoti <nikunjlahoti+udacityclouddev@gmail.com>
2020-05-18 19:16:13 +0530

    Allowing Cors

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/index.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/index.router.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from 'express';
 import { FeedRouter } from './feed/routes/feed.router';
 import { UserRouter } from './users/routes/user.router';
+// import { config } from '../../config/config';
+// import * as cors from 'cors';
 
 const router: Router = Router();
 
@@ -10,5 +12,19 @@ router.use('/users', UserRouter);
 router.get('/', async (req: Request, res: Response) => {    
     res.send(`V0`);
 });
+
+
+// //options for cors midddleware
+// const options:cors.CorsOptions = {
+//     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+//     credentials: true,
+//     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+//     origin: config.ui.cloudfront,
+//     preflightContinue: false
+//   };
+
+// router.use(cors(options))
+
+// router.options("*", cors(options));
 
 export const IndexRouter: Router = router;
